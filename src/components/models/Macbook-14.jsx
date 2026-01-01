571d80a0729feee279e2af06d3eef0e580725dfd
anish-devgit <xtylishanish@gmail.com>
2026-01-01 12:11:11 +0530

    comments: document UI component rendering hooks in Macbook-14.jsx

--- src/components/models/Macbook-14.jsx
+++ src/components/models/Macbook-14.jsx
@@ -14,7 +14,10 @@ import useMacbookStore from "../../store";
 import {noChangeParts} from "../../constants/index.js";
 import {Color, SRGBColorSpace} from 'three'
 
-export default function MacbookModel14(props) {
+/**
+ * UI state alignment and normalization check.
+ */
+export default functionexport default function MacbookModel14(props) {
     const { color } = useMacbookStore();
   const { nodes, materials, scene } = useGLTF('/models/macbook-14-transformed.glb');
 
